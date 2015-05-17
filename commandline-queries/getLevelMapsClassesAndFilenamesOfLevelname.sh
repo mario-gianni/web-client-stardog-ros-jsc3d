74a0c42b865e94db313c35251a67a051bc855208
Mario Gianni <gianni@dis.uniroma1.it>
2015-05-17 21:40:25 +0200

    dynamic select

---
+++ commandline-queries/getLevelMapsClassesAndFilenamesOfLevelname.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+export PATH=/Users/mario/Work/Projects/TRADR/stardog-3.0.1/bin:$PATH
+
+# give me the names of all the levels in the database
+stardog query tradr-domain "select distinct ?levelname ?class ?filename where {?level :hasLevelName ?levelname . ?levelmap a ?class . ?level :hasLevelMap ?levelmap . ?levelmap :hasLevelMapFilename ?filename filter strstarts(str(?levelname),'basement')}"
+
+# filter(!strstarts(str(?class),str(owl:)))
